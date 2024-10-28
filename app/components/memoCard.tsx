640cca3f9fd709661dde504b1e0f9afb7f3ef8a7
k-kamada <kamadak0404@gmail.com>
2024-10-28 16:08:28 +0900

    Add copy button

--- app/components/memoCard.tsx
+++ app/components/memoCard.tsx
@@ -9,25 +9,35 @@ export const MemoCard = (props: {
   isFocused: boolean
   setCurrentMemoId: React.Dispatch<React.SetStateAction<string | null>>
 }) => {
-  const borderColor = props.isFocused ? "border-sky-400" : "border-black"
+  const { memo, isFocused, setCurrentMemoId } = props
+  const borderColor = isFocused ? "border-sky-400" : "border-black"
+
+  const copyMemoToClipboard = async (e: React.MouseEvent<HTMLInputElement>) => {
+    e.stopPropagation()
+    const text = [memo.title, memo.body].join("\n").trim()
+    await navigator.clipboard.writeText(text)
+  }
+
   return (
     <button
-      id={props.memo.id}
+      id={memo.id}
       className={`bg-white border-2 ${borderColor} rounded-md p-2 text-inherit text-left cursor-default`}
-      onClick={() => props.setCurrentMemoId(props.memo.id)}
+      onClick={() => setCurrentMemoId(memo.id)}
       type="button"
     >
       <ManipulateBar
-        memoId={props.memo.id}
-        createdAt={props.memo.createdAt}
-        isArchived={props.memo.isArchived} />
+        memoId={memo.id}
+        createdAt={memo.createdAt}
+        isArchived={memo.isArchived}
+        copyToClipboard={copyMemoToClipboard}
+      />
       <hr />
-      <p className="py-1">{props.memo.title}</p>
-      {props.memo.body.length !== 0
+      <p className="py-1">{memo.title}</p>
+      {memo.body.length !== 0
         ? <div>
           <hr />
           <Linkify as="p" className="py-1 whitespace-pre-wrap">
-            {props.memo.body}
+            {memo.body}
           </Linkify>
         </div>
         : null
@@ -36,8 +46,13 @@ export const MemoCard = (props: {
   )
 }
 
-const ManipulateBar = (props: { memoId: string, createdAt: Date, isArchived: boolean }) => {
-  const { memoId, isArchived } = props
+const ManipulateBar = (props: {
+  memoId: string,
+  createdAt: Date,
+  isArchived: boolean,
+  copyToClipboard: (e: React.MouseEvent<HTMLInputElement>) => void,
+}) => {
+  const { memoId, isArchived, createdAt, copyToClipboard } = props
 
   // TODO: use modern DateTime library
   const generateDateTimeString = (date: Date): string => {
@@ -45,30 +60,40 @@ const ManipulateBar = (props: { memoId: string, createdAt: Date, isArchived: boo
   }
 
   return (
-    <div className="flex justify-start gap-2 items-center">
-      {!isArchived
-        ? (<div>
-          <Form // for "Memos" tab
-            action={`/api/archive/${memoId}`}
-            method="post"
-            className="flex justify-end"
-          >
-            <RoundedDangerButton type="submit">
-              Archive
-            </RoundedDangerButton>
-          </Form></div>)
-        : (<div>
-          <Form // for "Archived" tab
-            action={`/api/delete/${memoId}`}
-            method="post"
-            className="flex justify-end"
-          >
-            <RoundedDangerButton type="submit">
-              Delete
-            </RoundedDangerButton>
-          </Form></div>)
-      }
-      <p>{generateDateTimeString(props.createdAt)}</p>
+    <div className="flex justify-between">
+      {/* leftside items */}
+      <div className="flex justify-start gap-2 items-center">
+        {!isArchived
+          ? (<div>
+            <Form // for "Memos" tab
+              action={`/api/archive/${memoId}`}
+              method="post"
+              className="flex justify-end"
+            >
+              <RoundedDangerButton type="submit">
+                Archive
+              </RoundedDangerButton>
+            </Form></div>)
+          : (<div>
+            <Form // for "Archived" tab
+              action={`/api/delete/${memoId}`}
+              method="post"
+              className="flex justify-end"
+            >
+              <RoundedDangerButton type="submit">
+                Delete
+              </RoundedDangerButton>
+            </Form></div>)
+        }
+        <p>{generateDateTimeString(createdAt)}</p>
+      </div>
+      {/* rightside items */}
+      <button
+        type="submit"
+        className="border-2 border-blue-500 rounded-lg py-1 px-2"
+        onClick={copyToClipboard}>
+        Copy
+      </button>
     </div>
   )
 }
